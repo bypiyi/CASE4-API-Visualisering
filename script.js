710590cbb233590a362d96366600887e50c1541a
Alicia <23alicia.piyi@glimnet.se>
2024-01-28 18:33:10 +0200

    Added JS to starter-page

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // ----
+
+
+
+// FÖR VIDEON PÅ STARTSIDAN
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('myVideo');
+
+    // Sätta uppspelningshastigheten till 0.5 (halva normala hastigheten)
+    video.playbackRate = 0.5;
+});
