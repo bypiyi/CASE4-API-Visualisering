bb88fdc09775ee31c68e1a51c44ca982cc1ea4da
Alicia <23alicia.piyi@glimnet.se>
2024-02-05 21:52:22 +0200

    New changes to design

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
     // Sätta uppspelningshastigheten till 0.5 (halva normala hastigheten)
     video.playbackRate = 0.5;
 });
+
+
+
+// FÖR SÖKNING
+document.getElementById('searchIcon').addEventListener('click', function() {
+    var searchInput = document.querySelector('#searchBox input');
+    searchInput.removeAttribute('readonly');
+    searchInput.focus(); // Lägg fokus på sökrutan efter att ha klickat på sökikonen
+  });
+  
