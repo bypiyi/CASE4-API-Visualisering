b9c4e5e3156003b48a88fc9371eb467c919e0c91
Alicia <23alicia.piyi@glimnet.se>
2024-02-07 16:35:14 +0200

    New changes to result page

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-// ----
+let cameraString = window.location.href.split('?')[1];
+console.log(cameraString)
 
 
+// API
 
-// FÖR VIDEON PÅ STARTSIDAN
-document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('myVideo');
+fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=v2q9tdxOorB1liOooQgjTHN5v8tfWGkN26a6mBB7&"+cameraString) // Skicka request
+    .then(response => response.json()) // När svaret har kommit tillbaka tolka informationen i responsen
+    .then(data => {
+        // Arbete med data från vår request
 
-    // Sätta uppspelningshastigheten till 0.5 (halva normala hastigheten)
-    video.playbackRate = 0.5;
-});
+        console.log(data);
+
+        output = data.photos.filter(p => p.camera.name == "FHAZ");
+        urls = output.map(p => p.img_src);
 
 
+        let container = document.querySelector("#container");
+        container.innerHTML = ''
 
-// FÖR SÖKNING
-document.getElementById('searchIcon').addEventListener('click', function() {
-    var searchInput = document.querySelector('#searchBox input');
-    searchInput.removeAttribute('readonly');
-    searchInput.focus(); // Lägg fokus på sökrutan efter att ha klickat på sökikonen
-  });
-  
+        for (let i = 0; i < urls.length; i++) {
+            let img = document.createElement('img');
+            img.src = urls[i]
+            img.style.height = '200px';
+            img.style.width = '300px';
+            container.appendChild(img);
+        }
+
+    
+    });
